7a7b3be0ab5b5dac2d8e8a603eb0e1548d509fe5
Lora Elliott <128410600+loraavie@users.noreply.github.com>
2023-08-11 12:15:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+  var options = {
+    timeZone: 'America/Chicago', // Set the time zone to CST (Central Standard Time)
+    hour12: false, // Use 24-hour format
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
+  
+  document.getElementById('currentTime').innerHTML = new Date().toLocaleString('en-US', options);
 }
+
 showTime();
+
 setInterval(function () {
-	showTime();
+  showTime();
 }, 1000);
